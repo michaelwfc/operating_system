7916f8150301d7dcd4d785ec66f32ad3fbea9e07
michaelwfc <wangfeicheng007@gmail.com>
2025-11-07 07:04:18 +0800

    rename xv6_labs_2021 to xv6_labs_main

--- xv6_labs_main/kernel/sysproc.c
+++ xv6_labs_main/kernel/sysproc.c
@@ -48,8 +48,10 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  // ============ demo code: page faults for lazy allocation ============ 
+  myproc()->sz = myproc()->sz + n;
+  // if(growproc(n) < 0)
+  //   return -1;
   return addr;
 }
 
--- xv6_labs_main/kernel/vm.c
+++ xv6_labs_main/kernel/vm.c
@@ -335,8 +335,12 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
       panic("uvmunmap: walk");
+    
     if((*pte & PTE_V) == 0)
       panic("uvmunmap: not mapped");
+      // ============ demo code: page faults for lazy allocation ============ 
+      // when using lazy allocation, the page fault handler will allocate the page and update the PTE 
+      //  continue;
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
